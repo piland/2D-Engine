bb660dd5322abc72d584d22ac3cb0699b7599259
epiland <pilandethan@gmail.com>
2024-10-17 08:12:14 -0400

    Setup Basic Screen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    glfwSetErrorCallback(error_callback);
+
+    // Check for failed Initialization
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 840, "Hello World", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+    // Making the OpenGL context current
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+
+    }
+
+    // Terminate Window and Context
+    glfwDestroyWindow(window);
+    // Terminates GLFW
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
 }
